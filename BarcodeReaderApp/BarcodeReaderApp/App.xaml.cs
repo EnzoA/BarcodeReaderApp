1200a9cdd5a96ebe459082d7362ee16246325a0f
Enzo Andreetto <enzo.andreetto@gmail.com>
2018-10-08 21:27:31 -0300

    Install ZXing library

--- BarcodeReaderApp/BarcodeReaderApp/App.xaml.cs
+++ BarcodeReaderApp/BarcodeReaderApp/App.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+using BarcodeReaderApp.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,22 +11,7 @@ namespace BarcodeReaderApp
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
-		}
-
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
+			MainPage = new BarcodeReaderPage();
 		}
 	}
 }
---
+++ BarcodeReaderApp/BarcodeReaderApp/Pages/BarcodeReaderPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BarcodeReaderApp.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BarcodeReaderPage : ContentPage
+	{
+		public BarcodeReaderPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
