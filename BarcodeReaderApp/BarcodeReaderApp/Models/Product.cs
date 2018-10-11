7067ad93daef0b8e94a880c028e22908fea4ad9b
Enzo Andreetto <enzo.andreetto@gmail.com>
2018-10-11 07:49:23 -0300

    Create product model and add basic bindings in the barcode reader view model

---
+++ BarcodeReaderApp/BarcodeReaderApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace BarcodeReaderApp.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string CurrencySymbol { get; set; }
+
+        public double Price { get; set; }
+    }
+}
--- BarcodeReaderApp/BarcodeReaderApp/ViewModels/BarcodeReaderViewModel.cs
+++ BarcodeReaderApp/BarcodeReaderApp/ViewModels/BarcodeReaderViewModel.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace BarcodeReaderApp.ViewModels
 {
     public class BarcodeReaderViewModel : BindableBase
     {
+        ZXing.Result scanResult;
+        public ZXing.Result ScanResult
+        {
+            get => scanResult;
+            set => SetProperty(ref scanResult, value);
+        }
 
+        DelegateCommand onScanResultCommand;
+        public DelegateCommand OnScanResultCommand
+        {
+            get => onScanResultCommand ?? (onScanResultCommand = new DelegateCommand(OnScanResult));
+        }
+
+        void OnScanResult()
+        {
+
+        }
     }
 }
