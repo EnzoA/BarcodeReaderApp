8a452891acc4dfac9b9d051758b2ff62bef68448
Enzo Andreetto <enzo.andreetto@gmail.com>
2018-10-12 16:44:22 -0300

    Update packages

--- BarcodeReaderApp/BarcodeReaderApp/ViewModels/BarcodeReaderViewModel.cs
+++ BarcodeReaderApp/BarcodeReaderApp/ViewModels/BarcodeReaderViewModel.cs
@@ -1,22 +1,33 @@
-﻿using System;
-using Prism.Commands;
+﻿using Prism.Commands;
 using Prism.Mvvm;
 
 namespace BarcodeReaderApp.ViewModels
 {
     public class BarcodeReaderViewModel : BindableBase
     {
-        ZXing.Result scanResult;
+        ZXing.Result _scanResult;
         public ZXing.Result ScanResult
         {
-            get => scanResult;
-            set => SetProperty(ref scanResult, value);
+            get => _scanResult;
+            set => SetProperty(ref _scanResult, value);
         }
 
-        DelegateCommand onScanResultCommand;
+        bool _isScanning;
+        public bool IsScanning
+        {
+            get => _isScanning;
+            set => SetProperty(ref _isScanning, value);
+        }
+
+        DelegateCommand _onScanResultCommand;
         public DelegateCommand OnScanResultCommand
         {
-            get => onScanResultCommand ?? (onScanResultCommand = new DelegateCommand(OnScanResult));
+            get => _onScanResultCommand ?? (_onScanResultCommand = new DelegateCommand(OnScanResult));
+        }
+
+        public BarcodeReaderViewModel()
+        {
+
         }
 
         void OnScanResult()
